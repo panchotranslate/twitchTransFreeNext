183bbd9d0fba01ec7c97babb9d476dfe2e9f2bc2
panchotranslate <105177665+panchotranslate@users.noreply.github.com>
2022-05-08 17:49:03 -0500

    Update config.py

--- config.py
+++ config.py
@@ -1,8 +1,8 @@
 ######################################################
 # PLEASE CHANGE FOLLOWING CONFIGS ####################
-Twitch_Channel          = 'target_channel_name'
+Twitch_Channel          = 'CruzMode'
 
-Trans_Username          = 'trans_user_name'
+Trans_Username          = 'PanchoTranslate'
 Trans_OAUTH             = 'oauth_for_trans_user'
 
 #######################################################
@@ -10,21 +10,21 @@ Trans_OAUTH             = 'oauth_for_trans_user'
 Trans_TextColor         = 'GoldenRod'
 # Blue, Coral, DodgerBlue, SpringGreen, YellowGreen, Green, OrangeRed, Red, GoldenRod, HotPink, CadetBlue, SeaGreen, Chocolate, BlueViolet, and Firebrick
 
-lang_TransToHome        = 'ja'
+lang_TransToHome        = 'es'
 lang_HomeToOther        = 'en'
 
 Show_ByName             = True
 Show_ByLang             = True
 
-Ignore_Lang             = ['']
+Ignore_Lang             = ['en']
 Ignore_Users            = ['Nightbot', 'BikuBikuTest']
 Ignore_Line             = ['http', 'BikuBikuTest', '888', '８８８']
 Delete_Words            = ['saatanNooBow', 'BikuBikuTest']
 
 # Any emvironment, set it to `True`, then text will be read by TTS voice!
 # gTTS_In:User Input Text, gTTS_Out:Bot Output Text
-gTTS_In                 = True
-gTTS_Out                = True
+gTTS_In                 = False
+gTTS_Out                = False
 
 # if you make TTS for only few lang, please add langID in the list
 # for example, ['ja'] means Japanese only, ['ko','en'] means Korean and English are TTS!
